3239dc179b044ba88552195ceec43713c7a533e7
ben <bnsmcx@protonmail.ch>
2021-05-25 21:18:29 +0700

    inline bold/italic/code working, headings working

--- MarkdownToHTML.py
+++ MarkdownToHTML.py
@@ -8,19 +8,71 @@ PURPOSE:    Compiles Markdown documents to HTML.
                 getHTML()
                 getPreview()
 """
+import re
 
 
-def _process(line: str) -> str:
-    if line[0] == "\n":
+def _contains_inline_syntax(word: str) -> bool:
+    """check for bold/italic/code and return the right tags"""
+    return re.match("[`\\*]{1,3}", word)
+
+
+def _parse_inline_syntax(word: str) -> str:
+    """return appropriate html tag for code/bold/italics"""
+    if re.match("(?<!\\*)\\*(?!\\*)", word):
+        word = re.sub("^(?<!\\*)\\*(?!\\*)", "<em>", word)
+        word = re.sub("(?<!\\*)\\*(?!\\*)$", "</em>", word)
+        return word
+    elif re.match("(?<!\\*)\\*{2}(?!\\*)", word):
+        word = re.sub("^(?<!\\*)\\*{2}(?!\\*)", "<strong>", word)
+        word = re.sub("(?<!\\*)\\*{2}(?!\\*)$", "</strong>", word)
+        return word
+    elif re.match("(?<!\\*)\\*{3}(?!\\*)", word):
+        word = re.sub("^(?<!\\*)\\*{3}(?!\\*)", "<strong><em>", word)
+        word = re.sub("(?<!\\*)\\*{3}(?!\\*)$", "</em></strong>", word)
+        return word
+    elif re.match("`", word):
+        word = re.sub("^`", "<code>", word)
+        word = re.sub("`$", "</code>", word)
+        return word
+
+
+def _parse_heading(line: str) -> str:
+    """parse markdown to return html heading"""
+    heading_level = len(line.split()[0])
+    opening_tag = "<h" + str(heading_level) + ">"
+    heading_text = ""
+    closing_tag = "</h" + str(heading_level) + ">"
+
+    for word in line.strip().split()[1:]:
+        if _contains_inline_syntax(word):
+            heading_text += _parse_inline_syntax(word) + " "
+        else:
+            heading_text += word + " "
+
+    return opening_tag + heading_text.strip() + closing_tag
+
+
+def _process_line(line: str) -> str:
+    """parse a line of markdown to html"""
+    if line == '\n':
         return
-    if line[0] == "#":
-        line = line.strip("#").strip()
-        return "<h1>" + line + "</h1>"
-    elif line[0] == "<":
-        return "Process an html tag."
+
+    first_token = line.split()[0]
+
+    if re.fullmatch("#+", first_token):
+        return _parse_heading(line)
+    elif first_token == '>':
+        pass
+    elif first_token in ['-', '*']:
+        pass
+    elif re.fullmatch("[0-9]+\\.", first_token):
+        pass
+    elif re.fullmatch("[-=]{3,}", first_token):
+        pass
+    elif re.fullmatch("<.*", first_token):
+        pass
     else:
-        line = line.strip()
-        return "<p>" + line + "</p>"
+        pass
 
 
 def get_html(markdown_file_path: str) -> list:
@@ -29,7 +81,7 @@ def get_html(markdown_file_path: str) -> list:
     try:
         with open(markdown_file_path) as f:
             for line in f.readlines():
-                html.append(_process(line))
+                html.append(_process_line(line))
         return html
     except FileNotFoundError:
         return "File not found."
--- test.md
+++ test.md
@@ -1,6 +1,7 @@
-<!-- date = 22 May 2021 -->
 
 # This is the title of the post
-
-This is the body of text.  There are many interesting things being said here.  Most users are going to want to know what's going on.  Their interest is whetted.  Or peaked.  Or peeked?  Point is we should parse some of this paragraph to provide them a preview of the blog post.
+## This is *h2* in italics
+### This is **h3** in bold
+#### This is ***h4*** in bold and italic
+##### This is `h5` as code
 

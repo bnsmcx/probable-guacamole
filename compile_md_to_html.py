7a082882120e9246dc145a7099259723ad6c48cf
ben <bnsmcx@protonmail.ch>
2021-06-01 17:36:45 +0700

    added inline syntax and paragraph handling

--- compile_md_to_html.py
+++ compile_md_to_html.py
@@ -4,6 +4,7 @@ AUTHOR:     Ben Simcox
 PROJECT:    probable-guacamole
 PURPOSE:    Recursively compile markdown to html
 """
+import re
 
 
 class Compiler:
@@ -38,8 +39,10 @@ class Compiler:
             return ""
         if line[0] == "<!--":
             return self._parse_comment(line)
+        if line[0] in ['#', '##', '###', '####', '#####']:
+            return self._parse_heading(line, len(line[0]))
         else:
-            return ""
+            return self._parse_paragraph(line)
 
     def _parse_comment(self, line: list) -> str:
         """process an html comment"""
@@ -47,9 +50,11 @@ class Compiler:
         if len(line) == 0:
             line = self._get_next_line()
         token = line.pop(0)
-        print(token)
         if token != "-->":
-            comment += " " + token
+            if token == "<!--":
+                comment += token
+            else:
+                comment += " " + token
             comment += self._parse_comment(line)
         elif token == "-->":
             comment += " " + token + "\n"
@@ -58,3 +63,49 @@ class Compiler:
     def _get_next_line(self) -> list:
         """Get the next line of markdown, return it as a list"""
         return self.md_tokens.pop(0).split()
+
+    def _parse_heading(self, line: list, level: int) -> str:
+        """process an html heading"""
+        heading = "<h" + str(level) + ">"
+        line = line[1:]
+        while line:
+            token = line.pop(0)
+            heading += self._parse_inline_syntax(token) + " "
+        return heading.rstrip() + "</h" + str(level) + ">\n"
+
+    @staticmethod
+    def _parse_inline_syntax(token: str) -> str:
+        """check if a token contains markdown syntax"""
+        """Order of these statements is meaningful"""
+
+        """Handle bold and italic"""
+        if re.search("^\\*{3}.+\\*{3}$", token):
+            return "<bold><em>" + token[3:-3] + "</em></bold>"
+        if re.search("^\\*{3}", token):
+            return "<bold><em>" + token[3:]
+        if re.search("\\*{3}$", token):
+            return token[:-3] + "</em></bold>"
+
+        """Handle bold"""
+        if re.search("^\\*{2}.+\\*{2}$", token):
+            return "<bold>" + token[2:-2] + "</bold>"
+        if re.search("^\\*{2}", token):
+            return "<bold>" + token[2:]
+        if re.search("\\*{2}$", token):
+            return token[:-2] + "</bold>"
+
+        """Handle Italics"""
+        if re.search("^\\*.+\\*$", token):
+            return "<em>" + token[1:-1] + "</em>"
+        if re.search("^\\*", token):
+            return "<em>" + token[1:]
+        if re.search("\\*$", token):
+            return token[:-1] + "</em>"
+        return token
+
+    def _parse_paragraph(self, line: list) -> str:
+        """Parse markdown return html paragraph"""
+        paragraph_text = ""
+        for word in line:
+            paragraph_text += self._parse_inline_syntax(word) + " "
+        return "<p>" + paragraph_text.strip() + "</p>\n"
--- test.md
+++ test.md
@@ -3,3 +3,12 @@
 <!-- 
 another comment 
 -->
+
+# This is h1
+## This is h2
+### This is h3
+
+*italic*
+**bold**
+***bold and italic***
+
